9e8c3184a9bf78d49d6ea8cd37af7efa172f1ca6
Luan Luz <luanbrunoalvesluz@gmail.com>
2023-11-18 19:02:32 -0300

    WIP

--- landing-page/resources/js/sections/hero.js
+++ landing-page/resources/js/sections/hero.js
@@ -8,13 +8,13 @@ const heroTemplate = data => `
             ${data.subtitle}
         </p>
         <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-            <a href="https://suap.ifto.edu.br/eventos/inscricao/38/" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300">
+            <a href="#user-content-inscrições" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300">
                 Faça sua inscrição
                 <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                 </svg>
             </a>
-            <a href="https://si.ifto.edu.br/evento/certificados/" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-neutral-100 rounded-lg border border-gray-300 hover:text-gray-800 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100">
+            <a href="#user-content-certificado" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-neutral-100 rounded-lg border border-gray-300 hover:text-gray-800 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100">
                 <svg class="mr-2 -ml-1 w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 1v4a1 1 0 0 1-1 1H1m4 6 2 2 4-4m4-8v16a.97.97 0 0 1-.933 1H1.933A.97.97 0 0 1 1 18V5.828a2 2 0 0 1 .586-1.414l2.828-2.828A2 2 0 0 1 5.828 1h8.239A.97.97 0 0 1 15 2Z"/>
                 </svg>
