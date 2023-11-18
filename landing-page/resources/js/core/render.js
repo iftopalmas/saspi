96f1031d0616c0503e6508f2156cacd32c9a364a
Luan Luz <luanbrunoalvesluz@gmail.com>
2023-11-18 18:55:12 -0300

    WIP

--- landing-page/resources/js/core/render.js
+++ landing-page/resources/js/core/render.js
@@ -2,6 +2,7 @@ import {APP_TAG_SELECTOR} from "../consts.js"
 import {getHero} from "../sections/hero.js";
 import {getIntroduction} from "../sections/introduction.js";
 import {getGenericSection} from "../sections/genericSection.js";
+import {getOrganizingCommittee} from "../sections/organizingCommittee.js";
 
 export const renderSections = html => {
     const container = document.querySelector(APP_TAG_SELECTOR)
@@ -14,6 +15,7 @@ export const renderSections = html => {
     container.innerHTML += getGenericSection(html, '#user-content-bem-vindo-ao-ifto-campus-palmas')
     container.innerHTML += getGenericSection(html, '#user-content-inscrições')
     container.innerHTML += getGenericSection(html, '#user-content-certificado')
+    container.innerHTML += getOrganizingCommittee(html, '#user-content-comissão-organizadora')
 }
 
 export const renderTemplate = template => {
