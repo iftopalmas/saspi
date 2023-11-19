21df9db93764019c6425d6ae22ed830e80c55f80
Luan Luz <luanbrunoalvesluz@gmail.com>
2023-11-18 21:57:26 -0300

    WIP

--- docs/resources/js/consts.js
+++ docs/resources/js/consts.js
@@ -1,4 +1,4 @@
-export const GITHUB_REPO_URL = 'http://127.0.0.1:5500/index.html'
+export const GITHUB_REPO_URL = 'https://github.com/iftopalmas/saspi'
 export const README_CONTAINER_TAG_SELECTOR = '.markdown-body.entry-content.container-lg'
 export const APP_TAG_SELECTOR = '#app'
 
