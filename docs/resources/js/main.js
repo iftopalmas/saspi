9194b2c94a4d412ca8a4d15bf84316dc5a4578d8
Luan Luz <luanbrunoalvesluz@gmail.com>
2023-11-18 23:23:00 -0300

    WIP

--- docs/resources/js/main.js
+++ docs/resources/js/main.js
@@ -3,9 +3,9 @@ import {renderError} from "./sections/error.js"
 import {renderSections} from "./core/render.js"
 
 getContent()
-    .then(htmlString => {
+    .then(json => {
         const parser = new DOMParser()
-        return parser.parseFromString(htmlString, 'text/html')
+        return parser.parseFromString(json.contents, 'text/html')
     })
     .then(html => renderSections(html))
     .catch(error => {
