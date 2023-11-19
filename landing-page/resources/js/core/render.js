cd3aa0a5ed9d44474cab7cec00ccdd67adf4125d
Luan Luz <luanbrunoalvesluz@gmail.com>
2023-11-18 21:37:12 -0300

    WIP

--- landing-page/resources/js/core/render.js
+++ landing-page/resources/js/core/render.js
@@ -1,23 +1,24 @@
-import {APP_TAG_SELECTOR} from "../consts.js"
-import {getHero} from "../sections/hero.js";
-import {getIntroduction} from "../sections/introduction.js";
-import {getGenericSection} from "../sections/genericSection.js";
-import {getOrganizingCommittee} from "../sections/organizingCommittee.js";
-import {getEventSchedule} from "../sections/eventSchedule.js";
+import {APP_TAG_SELECTOR, README_CONTAINER_TAG_SELECTOR} from "../consts.js"
+import {getHero} from "../sections/hero.js"
+import {getIntroduction} from "../sections/introduction.js"
+import {getGenericSection} from "../sections/genericSection.js"
+import {getOrganizingCommittee} from "../sections/organizingCommittee.js"
+import {getEventSchedule} from "../sections/eventSchedule.js"
 
 export const renderSections = html => {
+    const effectiveHtml = html.querySelector(README_CONTAINER_TAG_SELECTOR)
     const container = document.querySelector(APP_TAG_SELECTOR)
     container.innerHTML = ''
 
-    container.innerHTML += getHero(html)
-    container.innerHTML += getIntroduction(html)
-    container.innerHTML += getGenericSection(html, '#user-content-objetivo-geral')
-    container.innerHTML += getGenericSection(html, '#user-content-objetivos-específicos')
-    container.innerHTML += getGenericSection(html, '#user-content-bem-vindo-ao-ifto-campus-palmas')
-    container.innerHTML += getEventSchedule(html, '#user-content-programação')
-    container.innerHTML += getGenericSection(html, '#user-content-inscrições')
-    container.innerHTML += getGenericSection(html, '#user-content-certificado')
-    container.innerHTML += getOrganizingCommittee(html, '#user-content-comissão-organizadora')
+    container.innerHTML += getHero(effectiveHtml)
+    container.innerHTML += getIntroduction(effectiveHtml)
+    container.innerHTML += getGenericSection(effectiveHtml, '#user-content-objetivo-geral')
+    container.innerHTML += getGenericSection(effectiveHtml, '#user-content-objetivos-específicos')
+    container.innerHTML += getGenericSection(effectiveHtml, '#user-content-bem-vindo-ao-ifto-campus-palmas')
+    container.innerHTML += getEventSchedule(effectiveHtml, '#user-content-programação')
+    container.innerHTML += getGenericSection(effectiveHtml, '#user-content-inscrições')
+    container.innerHTML += getGenericSection(effectiveHtml, '#user-content-certificado')
+    container.innerHTML += getOrganizingCommittee(effectiveHtml, '#user-content-comissão-organizadora')
 }
 
 export const renderTemplate = template => {
